87d47464542afca93bef081dbc03a128c53c1d32
Your Name <you@example.com>
2024-06-15 05:36:31 -0400

    Call API to Fetch Weather Data Using fetch()

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,19 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+
+    const lat = document.getElementById('lat').value;
+    const lon = document.getElementById('lon').value;
+    const apiKey = ''; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+    const weatherInfo = document.getElementById('weatherInfo');
+    weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                            <p>Temperature: ${data.main.temp} &#8451;</p>
+                            <p>Weather: ${data.weather[0].description}</p>`;
+    })
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
